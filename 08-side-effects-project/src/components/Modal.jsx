e1e2d61bc9871924d0a64b5db53fb0a6fd8e5e5c
vmcalderingamez <vmcalderingamez@gmail.com>
2024-04-06 19:00:08 -0400

    adding side effects project

---
+++ 08-side-effects-project/src/components/Modal.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+function Modal({ open, children }) {
+  const dialog = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialog.current.showModal();
+    }
+
+    if (!open) {
+      dialog.current.close();
+    }
+  }, [open]);
+
+  return createPortal(
+    <dialog className="modal" ref={dialog}>
+      {open ? children : null}
+    </dialog>,
+    document.getElementById("modal")
+  );
+}
+
+export default Modal;
