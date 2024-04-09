693baeb5395f896836190107e226fe29258a3e68
vmcalderingamez <vmcalderingamez@gmail.com>
2024-04-09 16:15:41 -0400

    optimization techniques finished

--- 10-optimization-techniques-project/vite.config.js
+++ 10-optimization-techniques-project/vite.config.js
@@ -5,3 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+
+// recuerda que puedes optimizar la velocidad de las aplicaciones de react usando millionjs
