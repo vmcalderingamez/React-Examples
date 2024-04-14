51772a1ce36ddcde16048f283d2d0b2d8a0d333e
vmcalderingamez <vmcalderingamez@gmail.com>
2024-04-14 12:49:07 -0400

    adding classes project

---
+++ 11-classes-project/src/components/Users.js
@@ -0,0 +1,45 @@
+import { Component } from "react";
+import User from "./User";
+
+import classes from "./Users.module.css";
+
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true,
+    };
+  }
+
+  componentDidUpdate() {
+    if (this.props.users.length === 0) {
+      throw new Error("No Users provided!");
+    }
+  }
+
+  toggleUsersHandler() {
+    this.setState({showUsers : !this.state.showUsers})
+  }
+  
+  render() {
+
+    const usersList = (
+      <ul>
+        {this.props.users.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? "Hide" : "Show"} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  }
+}
+
+export default Users;
