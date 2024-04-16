7e2fd8d87196aeaa3ce59cc681d534ed738d1c0b
vmcalderingamez <vmcalderingamez@gmail.com>
2024-04-16 14:53:55 -0400

    adding forms project & getting user inputs via state

---
+++ 14-forms-project/src/components/Login.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(
+      `User email: ${enteredValues.email} \nPassword: ${enteredValues.password}`
+    );
+  }
+
+  function handleInputChange(event) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [event.target.id]: event.target.value,
+    }));
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleInputChange}
+            value={enteredValues.email}
+          />
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handleInputChange}
+            value={enteredValues.password}
+          />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
