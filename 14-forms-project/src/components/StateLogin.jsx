55ae844be51d28b45434a03de3a19e7c8e08bcc8
vmcalderingamez <vmcalderingamez@gmail.com>
2024-04-16 21:01:25 -0400

    forms project finished

--- 14-forms-project/src/components/StateLogin.jsx
+++ 14-forms-project/src/components/StateLogin.jsx
@@ -1,46 +1,30 @@
-import { useState } from "react";
+import Input from "./Input";
+import { isEmail, isNotEmpty, hasMinLength } from "../util/validation.js";
+import { useInput } from "../hooks/useInput.js";
 
 export default function StateLogin() {
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
+  const {
+    value: emailValue,
+    handleInputBlur: handleEmailBlur,
+    handleInputChange: handleEmailChange,
+    hasError: emailHasError,
+  } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
 
-  const [didEdit, setDidEdit] = useState({
-    email: false,
-    password: false,
-  });
-
-  const emailIsInvalid =
-    didEdit.email && !enteredValues.email.includes("@");
+  const {
+    value: passwordValue,
+    handleInputBlur: handlePasswordBlur,
+    handleInputChange: handlePasswordChange,
+    hasError: passwordHasError,
+  } = useInput("", (value) => hasMinLength(value, 6));
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(
-      `User email: ${enteredValues.email} \nPassword: ${enteredValues.password}`
-    );
-    setEnteredValues({
-      email: "",
-      password: "",
-    });
-  }
 
-  function handleInputChange(event) {
-    setEnteredValues((prevValues) => ({
-      ...prevValues,
-      [event.target.id]: event.target.value,
-    }));
-    setDidEdit(prevEdit => ({
-      ...prevEdit,
-      [event.target.id]: false
-    }))
-  }
+    if (emailHasError || passwordHasError) {
+      return;
+    }
 
-  function handleInputBlur(event) {
-    setDidEdit(prevEdit => ({
-      ...prevEdit,
-      [event.target.id]: true
-    }))
+    console.log(`User email: ${emailValue} \nPassword: ${passwordValue}`);
   }
 
   return (
@@ -48,32 +32,27 @@ export default function StateLogin() {
       <h2>Login</h2>
 
       <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onBlur={handleInputBlur}
-            onChange={handleInputChange}
-            value={enteredValues.email}
-          />
-          <div className="control-error">
-            {emailIsInvalid && <p>Please enter a valid email. </p>}
-          </div>
-        </div>
+        <Input
+          label="Email"
+          id="email"
+          type="email"
+          name="email"
+          value={emailValue}
+          onBlur={handleEmailBlur}
+          onChange={handleEmailChange}
+          error={emailHasError && "Please enter a valid email."}
+        />
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onBlur={handleInputBlur}
-            onChange={handleInputChange}
-            value={enteredValues.password}
-          />
-        </div>
+        <Input
+          label="Password"
+          id="password"
+          type="password"
+          name="password"
+          value={passwordValue}
+          onBlur={handlePasswordBlur}
+          onChange={handlePasswordChange}
+          error={passwordHasError && "Please enter a valid password."}
+        />
       </div>
 
       <p className="form-actions">
