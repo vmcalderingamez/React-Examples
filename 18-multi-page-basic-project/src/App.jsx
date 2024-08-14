d34dfc0b1f55f6603c0d1b1bed98a816dddfb0f0
vmcalderingamez <vmcalderingamez@gmail.com>
2024-08-13 22:20:47 -0400

    basic multi page app done

--- 18-multi-page-basic-project/src/App.jsx
+++ 18-multi-page-basic-project/src/App.jsx
@@ -1,11 +1,50 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  // Route,
+  RouterProvider,
+} from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> },
+  // *ABSOLUTE PATHS:
+  // {
+  //   path: "/",
+  //   element: <RootLayout />,
+  //   errorElement: <ErrorPage />,
+  //   children: [
+  //     { path: "/", element: <HomePage /> },
+  //     { path: "/products", element: <ProductsPage /> },
+  //     { path: "/products/:productId", element: <ProductDetailPage /> },
+  //   ],
+  // },
+
+  // *RELATIVE PATS:
+  {
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        // { path: "", element: <HomePage /> },
+        { index: true, element: <HomePage /> },
+        { path: "products", element: <ProductsPage /> },
+        { path: "products/:productId", element: <ProductDetailPage /> },
+      ],
+    },
 ]);
+
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ 18-multi-page-basic-project/src/pages/ProductDetail.jsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom"
+
+function ProductDetailPage() {
+
+    const params = useParams();
+
+
+    return <><h1>Product details!</h1> <p>{params.productId}</p></>
+}
+
+export default ProductDetailPage
